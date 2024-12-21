c444ad59d67dfee54fbafad80bf0c5e5cbefe966
Nada Hassan <nada.h22425@gmail.com>
2024-12-21 21:38:09 +0200

    posts list ui

--- src/app/pages/post/posts/post-item/post-item.component.ts
+++ src/app/pages/post/posts/post-item/post-item.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, input } from '@angular/core';
+import { AvatarModule } from 'primeng/avatar';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-post-item',
   standalone: true,
-  imports: [],
+  imports: [
+    CardModule,
+    AvatarModule,
+    DatePipe
+  ],
   templateUrl: './post-item.component.html',
   styleUrl: './post-item.component.scss'
 })
 export class PostItemComponent {
-
+  card=input.required<any>()
 }
--- src/app/pages/post/posts/posts.component.ts
+++ src/app/pages/post/posts/posts.component.ts
@@ -1,12 +1,69 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Card } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { IconField } from 'primeng/iconfield';
+import { InputIcon } from 'primeng/inputicon';
 
+import { PostItemComponent } from "./post-item/post-item.component";
 @Component({
   selector: 'app-posts',
   standalone: true,
-  imports: [],
+  imports: [
+
+    RouterModule,
+    InputTextModule,
+    IconField,
+    InputIcon,
+    PostItemComponent
+  ],
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.scss'
 })
 export class PostsComponent {
+  cards = [
+    {
+      header: 'UX review presentations',
+      subheader: 'Design',
+      image: 'path-to-image-1.jpg',
+      description: 'How do you create compelling presentations that wow your colleagues and impress your manager?',
+      link: '/design/ux-review',
+    },
+    {
+      header: 'Migrating to Linear 101',
+      subheader: 'Product',
+      image: 'path-to-image-2.jpg',
+      description: 'Learn basic workflow software practices, quick tips, and hacks.',
+      link: '/product/migrating',
+    },
+    {
+      header: 'Building your API Stack',
+      subheader: 'Software Engineering',
+      image: 'path-to-image-3.jpg',
+      description: 'The tools RESTful APIs have been met by a rise in tools for modeling, testing, and building APIs.',
+      link: '/software/api-stack',
+    },
+    {
+      header: 'Building your API Stack',
+      subheader: 'Software Engineering',
+      image: 'path-to-image-3.jpg',
+      description: 'The tools RESTful APIs have been met by a rise in tools for modeling, testing, and building APIs.',
+      link: '/software/api-stack',
+    },
+    {
+      header: 'Building your API Stack',
+      subheader: 'Software Engineering',
+      image: 'path-to-image-3.jpg',
+      description: 'The tools RESTful APIs have been met by a rise in tools for modeling, testing, and building APIs.',
+      link: '/software/api-stack',
+    },
+    {
+      header: 'Building your API Stack',
+      subheader: 'Software Engineering',
+      image: 'path-to-image-3.jpg',
+      description: 'The tools RESTful APIs have been met by a rise in tools for modeling, testing, and building APIs.',
+      link: '/software/api-stack',
+    },
+  ];
 
 }
